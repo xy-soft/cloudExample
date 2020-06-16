711fe38273f8954b0e7a234c86673368ec80f03d
昕有灵犀 <475660@qq.com>
2020-06-16 13:18:42 +0800

    第一次提交

---
+++ firstSpringGradle/src/main/java/com/xin/firstSpringGradle/FirstSpringGradleApplication.java
@@ -0,0 +1,13 @@
+package com.xin.firstSpringGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringGradleApplication.class, args);
+	}
+
+}
